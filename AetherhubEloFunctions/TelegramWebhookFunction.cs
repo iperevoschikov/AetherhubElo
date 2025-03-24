9ea0765a015148166fb9ca61e1f15627bdb2d236
iperevoschikov <iperevoschikov@skbkontur.ru>
2025-03-24 14:55:21 +0500

    Boilerplate code

---
+++ AetherhubEloFunctions/TelegramWebhookFunction.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using YandexCloudFunctions.Net.Sdk;
+
+namespace AetherhubEloFunctions;
+
+public class TelegramWebhookFunction : BaseFunctionHandler
+{
+    protected override void ConfigureServices(IServiceCollection serviceCollection)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<FunctionHandlerResponse> Handle(FunctionHandlerRequest request)
+    {
+        return FunctionHandlerResponses.Ok();
+    }
+}
