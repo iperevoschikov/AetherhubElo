4c98eb40001c7d2920da0b26b78d5f7357688675
iperevoschikov <iperevoschikov@skbkontur.ru>
2025-03-25 13:38:00 +0500

    setup all dependencies and test command

--- AetherhubEloFunctions/TelegramWebhookFunction.cs
+++ AetherhubEloFunctions/TelegramWebhookFunction.cs
@@ -1,17 +1,70 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Google.Cloud.Firestore;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using Telegram.Bot;
+using Telegram.Bot.Types;
 using YandexCloudFunctions.Net.Sdk;
 
 namespace AetherhubEloFunctions;
 
 public class TelegramWebhookFunction : BaseFunctionHandler
 {
-    protected override void ConfigureServices(IServiceCollection serviceCollection)
+    // ReSharper disable once UnusedMember.Global
+    public static async Task<FunctionHandlerResponse> HandleAsync(
+        FunctionHandlerRequest request,
+        ITelegramBotClient botClient,
+        FirestoreDb firestoreDb)
     {
-        throw new NotImplementedException();
+        var update = JsonConvert.DeserializeObject<Update>(request.Body)!;
+        var message = update.Message;
+        if (message?.Text != null)
+        {
+            if (message.Text.StartsWith("/communixes"))
+            {
+                var documents = firestoreDb
+                    .Collection("communixes")
+                    .ListDocumentsAsync();
+                var communixes = new List<string>();
+                await foreach (var document in documents)
+                {
+                    communixes.Add((await document.GetSnapshotAsync()).GetValue<string>("name"));
+                }
+
+                await botClient.SendMessage(
+                    message.Chat.Id,
+                    string.Join('\n', communixes));
+            }
+        }
+
+        return FunctionHandlerResponses.Ok();
     }
 
-    public async Task<FunctionHandlerResponse> Handle(FunctionHandlerRequest request)
+    protected override void ConfigureServices(IServiceCollection services)
     {
-        return FunctionHandlerResponses.Ok();
+        var telegramAccessToken = GetConfigurationValue("TG_ACCESS_TOKEN");
+        var googleCloudJsonCredentials =
+            System.Text.Encoding.UTF8.GetString(
+                Convert.FromBase64String(
+                    GetConfigurationValue("GOOGLE_CLOUD_JSON_CREDENTIALS")));
+
+        services
+            .AddSingleton(
+                new FirestoreDbBuilder
+                    {
+                        ProjectId = "mtg-ekb-elo",
+                        JsonCredentials = googleCloudJsonCredentials,
+                    }
+                    .Build())
+            .AddSingleton<ITelegramBotClient>(new TelegramBotClient(telegramAccessToken));
+    }
+
+    private static string GetConfigurationValue(string name)
+    {
+        var value = Environment.GetEnvironmentVariable(name);
+
+        if (string.IsNullOrEmpty(value))
+            throw new Exception($"{name} not found");
+
+        return value;
     }
 }
