5318c4d8c6d2cb2cae2340f67a8a0c872ce6943e
iperevoschikov <iperevoschikov@skbkontur.ru>
2025-04-23 09:43:10 +0500

    feat(tests): boilerplate test for debug

---
+++ AetherhubEloFunctions.Tests/DebugTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace AetherhubEloFunctions.Tests;
+
+public class DebugTests
+{
+    [Test]
+    public async Task Debug()
+    {
+        var serviceProvider = new ServiceCollection()
+            .ConfigureStorage()
+            .BuildServiceProvider();
+
+        var tourneysStorage = serviceProvider.GetRequiredService<TourneysStorage>();
+
+        var tourneys = await tourneysStorage.GetTourneys().ToArrayAsync();
+        var pioneer = tourneys.Where(t => t.Communix == "pioneer")
+            .OrderByDescending(t => t.Date)
+            .ToArray();
+        Assert.That(pioneer, Has.Length.EqualTo(pioneer.Select(t => t.Date).Distinct().Count()));
+    }
+}
