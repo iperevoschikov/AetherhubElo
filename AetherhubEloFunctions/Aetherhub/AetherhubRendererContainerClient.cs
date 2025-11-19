607d8616c473c7535c98a2dcf93faaf8f7c7d484
iperevoschikov <iperevoschikov@skbkontur.ru>
2025-11-19 08:17:12 +0500

    wip

---
+++ AetherhubEloFunctions/Aetherhub/AetherhubRendererContainerClient.cs
@@ -0,0 +1,16 @@
+using AetherhubEloFunctions.YandexCloud;
+
+namespace AetherhubEloFunctions.Aetherhub;
+
+public class AetherhubRendererContainerClient(YandexCloudClient yandexCloudClient)
+{
+    private const string containerUrl =
+        "https://bbavmv503kd9ksfsd3c5.containers.yandexcloud.net/?url=https://aetherhub.com/Tourney/?search=EDINOROG_EKB";
+
+    public async Task<string> Render(string url, string locator)
+    {
+        var aetherhubRendererUri = $"{containerUrl}?url={url}&locator={locator}";
+        var html = await yandexCloudClient.InvokeServerlessContainer(aetherhubRendererUri);
+        return html;
+    }
+}
--- AetherhubEloFunctions/Aetherhub/AetherhubTourneyFetcher.cs
+++ AetherhubEloFunctions/Aetherhub/AetherhubTourneyFetcher.cs
@@ -2,13 +2,13 @@
 
 namespace AetherhubEloFunctions.Aetherhub;
 
-public class AetherhubTourneysFetcher(YandexCloudClient yandexCloudClient)
+public class AetherhubTourneyFetcher(AetherhubRendererContainerClient containerClient)
 {
-    public async IAsyncEnumerable<TourneyMeta> FetchRecentTourneys()
+    public async IAsyncEnumerable<TourneyMeta> FetchTourney(int id)
     {
         var aetherhubRendererUri =
             "https://bbavmv503kd9ksfsd3c5.containers.yandexcloud.net/?url=https://aetherhub.com/Tourney/?search=EDINOROG_EKB";
-        var html = await yandexCloudClient.InvokeServerlessContainer(aetherhubRendererUri);
+        var html = await containerClient.Render().InvokeServerlessContainer(aetherhubRendererUri);
         await foreach (var meta in AetherhubTourneysListParser.Parse(html))
             yield return meta;
     }
